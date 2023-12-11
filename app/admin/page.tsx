0bb8cc7dd4f6b4afe0b3b29ccb13ca38a0438152
BeshoyMorad <besh0morta@gmail.com>
2023-12-11 22:07:36 +0200

    Add the main pages structure

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPanel() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">Admin Panel</h1>
+    </main>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">
+        <span style={{ color: "#971818" }}>404</span> - Page Not Found
+      </h1>
+    </main>
+  );
+}
