7e9637cbc9ac7e0d1f7b71e62cf783189a46a985
BeshoyMorad <besh0morta@gmail.com>
2023-12-12 19:34:22 +0200

    Add admin panel navbar

---
+++ app/admin/layout.tsx
@@ -0,0 +1,48 @@
+import { AppBar, Box, Button, Toolbar, Container } from "@mui/material";
+import Link from "next/link";
+
+const navItems = [
+  {
+    title: "Home",
+    link: "admin",
+  },
+  {
+    title: "Manage All Users",
+    link: "admin/users",
+  },
+];
+
+export default function AdminPanelLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AppBar
+        component="nav"
+        position="static"
+        sx={{
+          bgcolor: "var(--admin-background-color)",
+          borderRadius: "1rem",
+          width: "97%",
+          margin: "3px auto",
+        }}
+      >
+        <Toolbar>
+          <h5 className="flex-1">Admin panel</h5>
+
+          {navItems.map((item) => (
+            <Button key={item.title} sx={{ color: "#fff" }}>
+              <Link href={`/${item.link}`}>{item.title}</Link>
+            </Button>
+          ))}
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="lg" className="mt-5">
+        {children}
+      </Container>
+    </>
+  );
+}
