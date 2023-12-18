32b9688bb6c7c4db38cb5b062964abd5e4b34a33
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-12-18 23:45:21 +0200

    add profile page

---
+++ Database/profile.js
@@ -0,0 +1,20 @@
+export const optionsRole = [
+  {
+    id: 1,
+    name: "Manager",
+  },
+  {
+    id: 2,
+    name: "Fan",
+  },
+];
+export const optionsGender = [
+  {
+    id: 1,
+    name: "Male",
+  },
+  {
+    id: 2,
+    name: "Female",
+  },
+];
