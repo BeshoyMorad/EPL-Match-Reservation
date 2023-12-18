fab5c6bf4bb2959184e77f2e8616687c7da83c55
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-12-18 20:40:02 +0200

    Check user approval middleware

--- backend/middlewares/authMiddlewares.js
+++ backend/middlewares/authMiddlewares.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { getUserById } from "../services/userServices.js";
 
 export function verifyAuthToken(req, res, next) {
   try {
@@ -9,8 +10,17 @@ export function verifyAuthToken(req, res, next) {
     req.payload = payload;
     next();
   } catch (err) {
-    res.status(401).json({
-      error: "Invalid Token",
-    });
+    res.status(401).json("Invalid Token");
+  }
+}
+
+export async function verifyApprovedUser(req, res, next) {
+  try {
+    const userId = req.payload?.userId;
+    const user = await getUserById(userId);
+    if (!user || !user.approved) throw new Error("Not approved");
+    next();
+  } catch (err) {
+    res.status(401).json("User is not yet approved");
   }
 }
