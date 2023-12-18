665f1ea90549b2061aa5ec5dd3068a5dead82e75
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-12-18 19:52:25 +0200

    Use match router

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRouter from "./auth.js";
+import matchRouter from "./matches.js";
 
 const mainRouter = express.Router();
 
 mainRouter.use(authRouter);
+mainRouter.use(matchRouter);
 
 mainRouter.use((req, res) => {
   res.status(404).json(`Can't ${req.method} ${req.url}`);
