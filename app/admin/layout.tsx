2db8b9ebd5bab85b6689ee28d1ae28b675dabf12
BeshoyMorad <besh0morta@gmail.com>
2023-12-21 21:23:26 +0200

    Finish admin panel

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,22 +1,21 @@
-import { AppBar, Box, Button, Toolbar, Container } from "@mui/material";
-import Link from "next/link";
+"use client";
 
-const navItems = [
-  {
-    title: "Home",
-    link: "admin",
-  },
-  {
-    title: "Manage All Users",
-    link: "admin/users",
-  },
-];
+import { Container } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
 
 export default function AdminPanelLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [cookies] = useCookies(["isAdmin"]);
+  const navigate = useRouter();
+
+  if (!cookies.isAdmin) {
+    navigate.push("/signin");
+  }
+
   return (
     <Container maxWidth="lg" className="mt-5">
       {children}
