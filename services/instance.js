4faeda64798fa7931cf639f2878020a2729d5b9d
BeshoyMorad <besh0morta@gmail.com>
2023-12-21 21:42:39 +0200

    Fix token issue

--- services/instance.js
+++ services/instance.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
-const TOKEN = window.document.cookie
-  .split(";")
-  .find((c) => c.trim().startsWith("token="))
-  .split("=")[1];
+
+let TOKEN = "";
+const cookie = document.cookie;
+if (cookie) {
+  TOKEN = cookie
+    .split(";")
+    .find((c) => c.trim().startsWith("token="))
+    .split("=")[1];
+}
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
