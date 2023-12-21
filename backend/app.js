5bce91bd4df8d9f99d066974916b013744b3f162
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-12-21 21:27:22 +0200

    Setting up socket io

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
-import multer from "multer";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import path from "path";
 import cors from "cors";
-import { fileURLToPath } from "url";
-import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
 import mainRouter from "./routes/router.js";
 import Team from "./models/Team.js";
 import teams from "./seeds/teams.js";
+import http from "http"
+import { Server }  from "socket.io"
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use("/images", express.static(path.join(__dirname, "images")));
 
 let DB_URL = process.env.MONGO_URL.trim();
 mongoose
@@ -44,36 +39,12 @@ mongoose
     console.log("Unable to connect to MongoDB:", error);
   });
 
-// app.use((_req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,POST,PUT,DELETE,PATCH,OPTIONS"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-//   next();
-// });
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_BASE_URL.trim(),
     credentials: true,
   })
 );
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "EPL",
-      version: "1.0.0",
-      description: "API-Documentation",
-    },
-  },
-  apis: ["./routes/*.js"],
-};
-const specs = swaggerJsDoc(options);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
 app.use(mainRouter);
 
 app.listen(port, () => {
